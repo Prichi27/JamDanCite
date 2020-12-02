da27309beff76940d894694a1cfd03c05e78a8ab
prichiSupinfo <264144@supinfo.com>
2020-12-03 01:33:11 +0400

    ASTC + 45 degreeeee

---
+++ Assets/Project/Scripts/Player/InputManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerPosition();
+
+
+    }
+    
+    /// <summary>
+    /// Get mouse input
+    /// </summary>
+    private void GetPlayerPosition()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = GetAngleFromPosition(mousePosition, _rigidBody.position);
+
+
+        _rigidBody.rotation = angle;
+
+        float skewedAngle = (angle + 45) * Mathf.Deg2Rad;
+
+        // The suspense is killing me :(
+        if (Mathf.Sign(Mathf.Sin(skewedAngle)) == 1 && Mathf.Sign(Mathf.Cos(skewedAngle)) == 1 && Mathf.Sign(Mathf.Tan(skewedAngle)) == 1)
+        {
+            // Back
+            Debug.Log("All");
+            //_spriteRenderer.sprite = Sprite 
+        }
+        else if (Mathf.Sign(Mathf.Sin(skewedAngle)) == 1)
+        {
+            // Left
+            Debug.Log("Sin");
+        }
+        else if (Mathf.Sign(Mathf.Tan(skewedAngle)) == 1)
+        {
+            // Front
+            Debug.Log("Tan");
+        }
+        else if (Mathf.Sign(Mathf.Cos(skewedAngle)) == 1)
+        {
+            // Right
+            Debug.Log("Cos");
+        }
+
+        //Debug.Log(transform.eulerAngles.z);
+
+    }
+
+    private float GetAngleFromPosition(Vector2 mouse, Vector2 player)
+    {
+        return Mathf.Atan2(player.y - mouse.y, player.x - mouse.x) * Mathf.Rad2Deg + 90f;
+    }
+
+}

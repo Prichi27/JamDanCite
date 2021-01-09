8764ab9df34e1178c36f5a96c96be222e2d16240
xxivxv <74407342+xxivxv@users.noreply.github.com>
2021-01-10 02:34:34 +0400

    Lightning go brrrrrr

---
+++ Assets/Project/Scripts/Powers/Fireball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : Projectile
+{
+    public override void SetPosition()
+    {
+        _currentPosition = transform.position;
+        _newPosition = _currentPosition + _velocity * Time.deltaTime;
+    }
+    
+    public override void SetVelocity(Vector2 velocity)
+    {
+        _velocity = velocity;
+    }
+
+    public override void SetRotation(float zAngle)
+    {
+        transform.Rotate(0, 0, zAngle);
+    }
+    
+    public override void HitEnemies()
+    {
+        RaycastHit2D[] hits = Physics2D.LinecastAll(_currentPosition + offset, _newPosition + offset);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            GameObject other = hit.collider.gameObject;
+
+            if (other.CompareTag(Constants.ENEMY_TAG))
+            {
+                OnEnemyDamaged.Raise(other.GetInstanceID());
+                OnEnemyDamaged.Raise();
+                gameObject.SetActive(false);
+                break;
+            }
+                
+            if (other.CompareTag(Constants.OBSTACLE_TAG))
+            {
+                gameObject.SetActive(false);
+                break;
+            }            
+        }
+
+        transform.position = _newPosition;
+    }
+
+}
---
+++ Assets/Project/Scripts/Powers/Lightning.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : Projectile
+{
+    [SerializeField] private FloatVariable _explosionForce;
+    [SerializeField] private Vector2Variable _playerPosition;
+
+    public override void SetPosition()
+    {
+        _currentPosition = transform.position;
+        _newPosition = _currentPosition + _velocity * Time.deltaTime;
+    }
+
+    public override void SetVelocity(Vector2 velocity)
+    {
+        return;
+    }
+    public override void SetRotation(float zAngle)
+    {
+        return;
+    }
+
+    public override void HitEnemies()
+    {
+        Vector2 explosionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, power.damageRadius);
+
+        foreach (Collider2D hit in colliders)
+        {
+            GameObject other = hit.gameObject;
+
+            if (other.CompareTag(Constants.ENEMY_TAG))
+            {                
+                Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+                Vector2 force = (Vector2)other.transform.position - (Vector2)_playerPosition.RuntimeValue;
+                rb.AddForce(force.normalized * _explosionForce.RuntimeValue, ForceMode2D.Impulse);
+                OnEnemyDamaged.Raise(other.GetInstanceID());
+                OnEnemyDamaged.Raise();
+            }
+        }
+
+        transform.position = _newPosition;
+
+        HasSpawned = true;
+    }
+}

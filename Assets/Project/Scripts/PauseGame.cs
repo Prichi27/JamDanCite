5a507434d5b54e4e1ac6d06843f14ff977a8ab45
prichiSupinfo <264144@supinfo.com>
2021-01-21 02:53:50 +0400

    have a break, have a kit kat

---
+++ Assets/Project/Scripts/PauseGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private BoolVariable _isPlayerDead;
+    [SerializeField] private BoolVariable _isGamePaused;
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if((Input.GetKeyDown(KeyCode.P) || (Input.GetKeyDown(KeyCode.Space))) && !_isPlayerDead.RuntimeValue)
+        {
+            if(pausePanel.activeSelf )
+            {
+                UnPause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        _isGamePaused.RuntimeValue = true;
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        _isGamePaused.RuntimeValue = false;
+    }
+}
---
+++ Assets/Project/Scripts/ScriptDisabler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDisabler : MonoBehaviour
+{
+    [SerializeField] private BoolVariable _isGamePaused;
+
+    private MonoBehaviour[] scripts;
+    private bool _isDisabled;
+
+    private void Awake()
+    {
+        scripts = GetComponents<MonoBehaviour>();
+    }
+
+    private void Update()
+    {
+        if(_isGamePaused.RuntimeValue && !_isDisabled)
+        {
+            // Disable Scripts
+            foreach (MonoBehaviour script in scripts)
+            {
+                if(script.GetType() != typeof(ScriptDisabler))
+                {
+                    script.enabled = false;
+                }
+            }
+
+            _isDisabled = true;
+        }
+
+        else if (!_isGamePaused.RuntimeValue && _isDisabled)
+        {
+            foreach (MonoBehaviour script in scripts)
+            {
+                if (script.GetType() != typeof(ScriptDisabler))
+                {
+                    script.enabled = true;
+                }
+            }
+
+            _isDisabled = false;
+        }
+    }
+}
